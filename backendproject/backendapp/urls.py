dfa3eadebb335b568dd266733d6ade6db5955198
tino <tinomadrid77@gmail.com>
2026-01-17 18:31:33 -0600

    Docker files, corrected view urls

--- backendproject/backendapp/urls.py
+++ backendproject/backendapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('jobs/', views.job_list, name='job-list'),
-    path('jobs/', views.job_create, name='job-create'),  # POST to same path
-    path('jobs/<int:pk>/', views.job_update, name='job-update'),
-    path('jobs/<int:pk>/', views.job_delete, name='job-delete'),
+    path('jobs/', views.jobList, name='job-list'),
+    path('jobs/', views.jobCreate, name='job-create'),  # POST to same path
+    path('jobs/<int:pk>/', views.jobUpdate, name='job-update'),
+    path('jobs/<int:pk>/', views.jobDelete, name='job-delete'),
 ]
